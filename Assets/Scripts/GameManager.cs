ff6eb78ccff7d427969410e14937b4a85f647bdc
camilo1962 <108455047+camilo1962@users.noreply.github.com>
2024-03-20 09:04:13 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public GameObject panelGameOver;
+
+
+    // Start is called before the first frame update
+    public static GameManager instance;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+    public void RestartGame()
+    {
+        SoundManager.instance.Gameoversound();
+        Invoke("RestartAfterTime", 1f);
+        SoundManager.instance.Gameoversound();
+    }
+    void RestartAfterTime()
+    {
+        panelGameOver.SetActive(true);
+        //UnityEngine.SceneManagement.SceneManager.LoadScene("Gamescene");
+        SoundManager.instance.GameOverSonido();
+    }
+    void Start()
+    {
+        panelGameOver.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+
+    public void Otra()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Gamescene");
+    }
+}
+
+
+
+
+
+
---
+++ Assets/Scripts/Playerscript.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.Impl;
+using static UnityEngine.GraphicsBuffer;
+
+public class Playerscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Rigidbody2D myBody;
+    public float moveSpeed = 2f;
+    private int score;
+    public TMP_Text scoreText, recordText;
+    private BoxCollider2D box;
+
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+        box =  GetComponent<BoxCollider2D>();
+    }
+
+    void Start()
+    {
+        score = 0;
+        recordText.text = PlayerPrefs.GetInt("record", 0).ToString();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+    }
+    void Move()
+    {
+        if(Input.GetAxisRaw("Horizontal")>0f)
+        {
+            myBody.velocity = new Vector2(moveSpeed, myBody.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myBody.velocity = new Vector2(-moveSpeed, myBody.velocity.y);
+        }
+    }
+    public void PlatformMove(float x)
+    {
+        myBody.velocity = new Vector2(x, myBody.velocity.y);
+    }
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.collider.CompareTag("plataforma"))
+        {
+
+            score++;
+            PlayerPrefs.SetInt("record", 0);
+            scoreText.text = score.ToString();
+        }
+        if (other.collider.CompareTag("plataforma1"))
+        {
+
+            score = score + 4; ;
+        }
+        if (other.collider.CompareTag("plataforma2"))
+        {
+
+            score = score + 2; ;
+        }
+    }
+
+
+    public void Update()
+    {
+        scoreText.text = score.ToString();
+        if(score > PlayerPrefs.GetInt("record", 0))
+        {
+            PlayerPrefs.SetInt("record", score);
+            //recordText.text = PlayerPrefs.GetInt("record", 0).ToString();
+        }
+    }
+
+}
